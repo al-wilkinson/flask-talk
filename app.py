5cb9e2ec89b5d2fb53419470baaabe6cdf8a13f1
Al Wilkinson <alastair.wilkinson@wkconsulting.com.au>
2024-12-28 05:45:10 +1000

    Use jsonify for status, not smorest

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from fake_data import generate_test_data
 from kvt import get_kvt_secret
-from flask import Flask
-from flask_smorest import abort
+from flask import Flask, jsonify
 from mi_token import get_mi_token, get_env_var
 
 app = Flask(__name__)
@@ -9,14 +8,9 @@ app = Flask(__name__)
 KVUri = f"https://kvt-demo-01.vault.azure.net/"
 secretName = "SuperSecret"
 
-def find_person(xnumber):
-    return test_data[xnumber]
-
 test_data = generate_test_data(10)
 for person in test_data:
-    print(f"{person} {test_data[person]}")
-
-print(find_person("x2950000"))  
+    print(f"{person} {test_data[person]}") 
 
 @app.route('/get-person/<string:xnumber>', methods=['GET'])
 def get_person(xnumber):
@@ -24,7 +18,7 @@ def get_person(xnumber):
         return test_data[xnumber]
     except:
         print("Person not found")
-        abort(404, message="Person not found")
+        return jsonify(message="Person not found."), 404
 
 @app.route('/get-all', methods=['GET'])
 def get_all():
