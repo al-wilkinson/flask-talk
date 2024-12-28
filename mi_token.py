2b0367064e2657a851ef12f199009ca6aa296278
Al Wilkinson <alastair.wilkinson@wkconsulting.com.au>
2024-12-29 09:41:22 +1000

    Fix error checking in get_mi_token function..

--- mi_token.py
+++ mi_token.py
@@ -4,21 +4,23 @@ from flask import jsonify
 
 def get_env_var():
     try:
-        return jsonify(message=os.environ["IDENTITY_HEADER"]), 200
+        #return jsonify(message=os.environ["IDENTITY_HEADER"]), 200
+        return jsonify(message=os.environ["IDENTITY_ENDPOINT"]), 200
     except:
         return jsonify(message="Could not find IDENTITY_HEADER"), 404
 
 def get_mi_token():
-    url = "http://169.254.169.254/metadata/identity/oauth2/token"
-    headers = {
-        "Metadata": "true"
-    }
     params = {
-        "api-version": "2018-02-01",
+        "api-version": "2019-08-01",
         "resource": "https://vault.azure.net"
     }
 
     try:
+        # all the os.environ calls need to be within error checking
+        url = os.environ["IDENTITY_ENDPOINT"]
+        headers = {
+        "X-IDENTITY-HEADER": os.environ["IDENTITY_HEADER"]
+    }
         # Make the GET request
         response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()  # Raise an error for bad status codes
@@ -26,10 +28,10 @@ def get_mi_token():
         # Parse and print the JSON response
         token_response = response.json()
         #print("Access Token:", token_response.get("access_token"))
-        return token_response.get("access_token")
+        return jsonify(message=token_response.get("access_token")), 200
 
-    except requests.exceptions.RequestException as e:
-        return(f"Something suboptimal happened: {e}")
+    except:
+        return jsonify(message="Something suboptimal happened. Have you configured the Managed Identity?"), 403
 
 if __name__ == "__main__":
     #print(get_mi_token())
